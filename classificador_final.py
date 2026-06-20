e447df5f2c90c9b3ee2a86d903f6ddd0196633dd
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-06-20 18:16:45 -0300

    feature: add aprendizado de decisão

--- classificador_final.py
+++ classificador_final.py
@@ -24,3 +24,7 @@ X = df.drop('nivel_suporte', axis=1)
 y = df['nivel_suporte']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size = 0.3, random_state = 1)
+
+# aprendizado
+clf = tree.DecisionTreeClassifier()
+clf.fit(X_train, y_train)
